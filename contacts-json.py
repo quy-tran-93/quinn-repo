e3fb31fe14bfa2ba22770c7d820d2ed21da0648e
quinn <tranthiquy2109@gmail.com>
2026-04-02 06:35:24 +0000

    add email field

--- contacts-json.py
+++ contacts-json.py
@@ -7,9 +7,9 @@ print("Content-Type: application/json\n")
 conn = pymysql.connect(host="localhost", user="quinn", password="123456", database="contacts_db")
 cur = conn.cursor()
 
-cur.execute("SELECT name, telephone FROM contacts")
+cur.execute("SELECT name, telephone, email FROM contacts")
 
-data = [{"name": r[0], "telephone": r[1]} for r in cur.fetchall()]
+data = [{"name": r[0], "telephone": r[1], "email": r[2]} for r in cur.fetchall()]
 
 print(json.dumps({
     "ok": True,
